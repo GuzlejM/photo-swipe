8699ea72c56a1327793c7d940c63b32f99b61ef7
guzlej <martin.guzlej@gmail.com>
2021-01-12 10:18:55 +0000

    it WORKS !

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,21 @@
-import Controller from '@ember/controller';
+import Ember from 'ember';
 
-export default class ApplicationController extends Controller {
-}
+export default Ember.Controller.extend({
+  items: [
+    {
+      src: 'https://picsum.photos/1024/768?image=42',
+      w: 1024,
+      h: 768,
+    },
+    {
+      src: 'https://picsum.photos/768/1024?image=80',
+      w: 768,
+      h: 1024,
+    },
+    {
+      src: 'https://picsum.photos/768/768?image=108',
+      w: 768,
+      h: 768,
+    },
+  ]
+});
